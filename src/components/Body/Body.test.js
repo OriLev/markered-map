adea99c79b53d6789532bfa86d75b7458ce79483
Ori Levitan <orilevitan2@gmail.com>
2019-09-14 13:56:41 +0300

    remove unecessary props

--- src/components/Body/Body.test.js
+++ src/components/Body/Body.test.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Body from './Body';
-import RouterStore from '../../stores/RouterStore';
-import AppStore from '../../stores/AppStore';
 
 it('renders without crashing', () => {
-  shallow(<Body router={new RouterStore(new AppStore())} />);
+  shallow(<Body />);
 });
