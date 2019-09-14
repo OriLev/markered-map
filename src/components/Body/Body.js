3ecbe8dbe0b983c962d223a9709ca714cf12b445
Ori Levitan <orilevitan2@gmail.com>
2019-09-14 16:12:12 +0300

    move MapDisplay to a separate component

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types';
 import { observer, inject } from 'mobx-react';
 import { withStyles } from '@material-ui/core/styles';
 // import Grid from '@material-ui/core/Grid';
-import { ReactBingmaps } from 'react-bingmaps';
 import Paper from '@material-ui/core/Paper';
+import MapDisplay from '../MapDisplay';
 
 const styles = theme => ({
   root: {
@@ -22,45 +22,41 @@ const styles = theme => ({
   container: {
     listStyle: 'none',
   },
-  map: {
-    width: '65%',
-  },
   markersDash: {
     width: '35%',
     color: 'black',
   },
 });
 
-const MapDisplay = ({ className }) => (
-  <ReactBingmaps
-    bingmapKey="AqnGWdRb63XPpubpT9NyFKou487EFaJm3wd7j4qBpJrFMGfZEYgqSg6rOSK6ZK9I"
-    className={className}
-  />
-);
-
-MapDisplay.propTypes = {
-  className: PropTypes.string.isRequired,
-};
-
-const MarkersDashBoard = ({ className }) => (
-  <div className={className}>Markers Dashboard to be build here</div>
+const MarkersDashBoard = ({ className, data }) => (
+  <div className={className}>
+    <ul>
+      {data.markers.map(marker => (
+        <li>
+          <span>{`lon: ${marker.lon} lat: ${marker.lat} name: ${marker.name}`}</span>
+        </li>
+      ))}
+    </ul>
+  </div>
 );
 
 MarkersDashBoard.propTypes = {
   className: PropTypes.string.isRequired,
+  data: PropTypes.shape({}).isRequired,
 };
 
-const Body = ({ classes }) => (
+const Body = ({ classes, data }) => (
   <div className={classes.root}>
     <Paper className={classes.paper}>
-      <MarkersDashBoard className={classes.markersDash} />
-      <MapDisplay className={classes.map} />
+      <MarkersDashBoard className={classes.markersDash} data={data} />
+      <MapDisplay />
     </Paper>
   </div>
 );
 
 Body.propTypes = {
   classes: PropTypes.shape({}).isRequired,
+  data: PropTypes.shape({}).isRequired,
 };
 
 export default inject('data')(withStyles(styles)(observer(Body)));
---
+++ src/components/MapDisplay/MapDisplay.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { observer, inject } from 'mobx-react';
+import { withStyles } from '@material-ui/core/styles';
+// import Grid from '@material-ui/core/Grid';
+import { ReactBingmaps } from 'react-bingmaps';
+// import Paper from '@material-ui/core/Paper';
+// import BookCard from '../BookCard';
+
+const styles = () => ({
+  root: {
+    flexGrow: 1,
+  },
+  map: {
+    width: '65%',
+  },
+});
+
+const MapDisplay = ({ classes, data }) => (
+  <ReactBingmaps
+    bingmapKey="AqnGWdRb63XPpubpT9NyFKou487EFaJm3wd7j4qBpJrFMGfZEYgqSg6rOSK6ZK9I"
+    className={classes.map}
+    pushPins={data.markers.map(marker => ({
+      location: [marker.lon, marker.lat],
+      option: { color: 'red' },
+    }))}
+    polyline={{
+      location: data.polyLinesLocation,
+      option: { strokeColor: 'blue', strokeThickness: 5 },
+    }}
+      // [
+      //   [32.092446, 34.782679],
+      //   [29.556066513061523, 34.950893402099609],
+      //   [31.951799392700195, 35.9404182434082],
+      //   [32.092446, 34.782679],
+      // ],
+    // center={[31.951799392700195, 35.9404182434082]}
+    center={data.center}
+    zoom={5}
+  />
+);
+
+MapDisplay.propTypes = {
+  classes: PropTypes.shape({}).isRequired,
+  data: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
+};
+
+export default inject('data')(withStyles(styles)(observer(MapDisplay)));
