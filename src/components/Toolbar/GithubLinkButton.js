7f7ccbf3f1d32a7404b174ee228ff3a48c4edd9a
Ori Levitan <orilevitan2@gmail.com>
2019-09-15 11:16:26 +0300

    add browser utility for navigation

--- src/components/Toolbar/GithubLinkButton.js
+++ src/components/Toolbar/GithubLinkButton.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import IconButton from '@material-ui/core/IconButton';
 import Github from '../Icons/Github';
-import { navigateTo } from '../../utils/browser';
+import navigateTo from '../../utils/browser';
 
 const GithubLinkButton = () => (
   <IconButton
