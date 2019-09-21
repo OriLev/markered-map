7b3de9893c8585df74b14727334309e59980ec71
Ori Levitan <orilevitan2@gmail.com>
2019-09-21 18:08:14 +0300

    Updates

---
+++ precache-manifest.8fedd5ff3b1db836fe2e71b4198a8f19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "968697ebd866783e77949eb16cd5b6cf",
+    "url": "/markered-map/index.html"
+  },
+  {
+    "revision": "e21e8b48cc5ee804450b",
+    "url": "/markered-map/static/css/2.711045af.chunk.css"
+  },
+  {
+    "revision": "55ed403402406e71426a",
+    "url": "/markered-map/static/css/main.9e855d66.chunk.css"
+  },
+  {
+    "revision": "e21e8b48cc5ee804450b",
+    "url": "/markered-map/static/js/2.2357d942.chunk.js"
+  },
+  {
+    "revision": "55ed403402406e71426a",
+    "url": "/markered-map/static/js/main.e20d2910.chunk.js"
+  },
+  {
+    "revision": "d85f14b7ada10cdc2e51",
+    "url": "/markered-map/static/js/runtime~main.5a9c6bd4.js"
+  }
+]);
